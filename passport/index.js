20d536f66a76ac5dbda2000a39e163575fd4a319
sihun <thrchtlgns@naver.com>
2022-08-10 21:18:51 +0900

    [feat] user,post 라우터 추가

--- passport/index.js
+++ passport/index.js
@@ -9,10 +9,19 @@ module.exports = () => {
   });
 
   passport.deserializeUser((id, done) => {
-    User.findOne({ where: { id } })
-      .then(user => {
-        done(null, user)
+    User.findOne({
+      where: { id },
+      include: [{
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followers',
+      }, {
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followings',
+      }],
     })
+      .then(user => done(null, user))
       .catch(err => done(err));
   });
 
